13f2500b69b334e7bf4f13e77af3cc84b900666d
Brendan Urban <bsurban7@gmail.com>
2022-02-22 21:42:14 -0800

    js addition 3

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
 'use strict'
 
 let username = prompt('What is your name?').toUpperCase();
-alert('Hi' + username + 'lets play a game!');
+console.log('Hi' + username + 'lets play a game!');
 
 let whereImFrom = prompt('Was I born in Washington?').toLowerCase();
 if (whereImFrom === 'yes' || whereImFrom === 'y') {
-    alert('No, I was actually born in California.');
+    console.log('No, I was actually born in California.');
     } else if (whereImFrom === 'no' || whereImFrom === 'n') {
-        alert('Correct, I was actually born in California');
+        console.log('Correct, I was actually born in California');
 } else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    console.log('You need to answer with a \'yes\' or a \'no\'');
 
 }
 
 
 let howOldAmI = prompt('Am I older than 24?').toLowerCase();
 if (howOldAmI === 'yes' || howOldAmI === 'y') {
-    alert('Sorry, I am actually exactly 24 years old');
+    console.log('Sorry, I am actually exactly 24 years old');
     } else if (howOldAmI === 'no' || howOldAmI === 'n') {
-        alert('Correct, I am exactly 24 years old');
+        console.log('Correct, I am exactly 24 years old');
 } else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    console.log('You need to answer with a \'yes\' or a \'no\'');
 
 }
 
 
 let myFavoriteSport = prompt('Is baseball the best sport?').toLowerCase();
 if (myFavoriteSport === 'yes' || myFavoriteSport === 'y') {
-    alert('Correct! It is the best sport and my favorite');
+    console.logt('Correct! It is the best sport and my favorite');
     } else if (myFavoriteSport === 'no' || myFavoriteSport === 'n') {
-        alert('You are incorrect, do you play soccer or something?');
+        console.log('You are incorrect, do you play soccer or something?');
 } else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    console.log('You need to answer with a \'yes\' or a \'no\'');
 
 }
 
 
 let theBestSeason = prompt('Is winter my favorite seasn?').toLowerCase();
 if (theBestSeason === 'yes' || theBestSeason === 'y') {
-    alert('Correct! Pray for snow!');
+    console.log('Correct! Pray for snow!');
     } else if (theBestSeason === 'no' || theBestSeason === 'n') {
-        alert('That is incorrect. Winter is the superior season');
+        console.log('That is incorrect. Winter is the superior season');
 } else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    console.log('You need to answer with a \'yes\' or a \'no\'');
 
 }
 
 
 let birthMonth = prompt('Was I born in December?').toLowerCase();
 if (birthMonth === 'yes' || birthMonth === 'y') {
-    alert('Correct! Two days before Christmas!');
+    console.log('Correct! Two days before Christmas!');
     } else if (birthMonth === 'no' || birthMonth === 'n') {
-        alert('Sorry, that is incorrect. I was born in December, just two days before Christmas!');
+        console.log('Sorry, that is incorrect. I was born in December, just two days before Christmas!');
 } else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    console.log('You need to answer with a \'yes\' or a \'no\'');
 
 }
 
