01ba3346b66514bd2cdc90e781977357c0915ff8
Charles <csipe84@gmail.com>
2022-02-23 19:16:10 -0800

    add howoldami function

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 'use strict'
 
-function username(){
-let username = prompt('What is your name?').toUpperCase();
-alert('Hi' + username + 'lets play a game!');
+function username() {
+    let username = prompt('What is your name?').toUpperCase();
+    alert('Hi' + username + 'lets play a game!');
 }
 username();
 
-function whereImFrom(){
-let whereImFrom = prompt('Was I born in Washington?').toLowerCase();
-if (whereImFrom === 'yes' || whereImFrom === 'y') {
-    alert('No, I was actually born in California.');
+function whereImFrom() {
+    let whereImFrom = prompt('Was I born in Washington?').toLowerCase();
+    if (whereImFrom === 'yes' || whereImFrom === 'y') {
+        alert('No, I was actually born in California.');
     } else if (whereImFrom === 'no' || whereImFrom === 'n') {
         alert('Correct, I was actually born in California');
-} else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    } else {
+        alert('You need to answer with a \'yes\' or a \'no\'');
 
-}
+    }
 }
 whereImFrom();
 
-let howOldAmI = prompt('Am I older than 24?').toLowerCase();
-if (howOldAmI === 'yes' || howOldAmI === 'y') {
-    alert('Sorry, I am actually exactly 24 years old');
+function howOldAmI() {
+    let howOldAmI = prompt('Am I older than 24?').toLowerCase();
+    if (howOldAmI === 'yes' || howOldAmI === 'y') {
+        alert('Sorry, I am actually exactly 24 years old');
     } else if (howOldAmI === 'no' || howOldAmI === 'n') {
         alert('Correct, I am exactly 24 years old');
-} else {
-    alert('You need to answer with a \'yes\' or a \'no\'');
+    } else {
+        alert('You need to answer with a \'yes\' or a \'no\'');
 
+    }
 }
-
+howOldAmI();
 
 let myFavoriteSport = prompt('Is baseball the best sport?').toLowerCase();
 if (myFavoriteSport === 'yes' || myFavoriteSport === 'y') {
     alert('Correct! It is the best sport and my favorite');
-    } else if (myFavoriteSport === 'no' || myFavoriteSport === 'n') {
-        alert('You are incorrect, do you play soccer or something?');
+} else if (myFavoriteSport === 'no' || myFavoriteSport === 'n') {
+    alert('You are incorrect, do you play soccer or something?');
 } else {
     alert('You need to answer with a \'yes\' or a \'no\'');
 
@@ -44,8 +46,8 @@ if (myFavoriteSport === 'yes' || myFavoriteSport === 'y') {
 let theBestSeason = prompt('Is winter my favorite seasn?').toLowerCase();
 if (theBestSeason === 'yes' || theBestSeason === 'y') {
     alert('Correct! Pray for snow!');
-    } else if (theBestSeason === 'no' || theBestSeason === 'n') {
-        alert('That is incorrect. Winter is the superior season');
+} else if (theBestSeason === 'no' || theBestSeason === 'n') {
+    alert('That is incorrect. Winter is the superior season');
 } else {
     alert('You need to answer with a \'yes\' or a \'no\'');
 
@@ -55,8 +57,8 @@ if (theBestSeason === 'yes' || theBestSeason === 'y') {
 let birthMonth = prompt('Was I born in December?').toLowerCase();
 if (birthMonth === 'yes' || birthMonth === 'y') {
     alert('Correct! Two days before Christmas!');
-    } else if (birthMonth === 'no' || birthMonth === 'n') {
-        alert('Sorry, that is incorrect. I was born in December, just two days before Christmas!');
+} else if (birthMonth === 'no' || birthMonth === 'n') {
+    alert('Sorry, that is incorrect. I was born in December, just two days before Christmas!');
 } else {
     alert('You need to answer with a \'yes\' or a \'no\'');
 
